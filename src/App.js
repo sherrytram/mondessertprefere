80d1405c9cda65ca2c9b904a4b430f9270ffe89d
Sherry Tram <sherrymtram@gmail.com>
2021-04-22 15:51:14 -0700

    flexbox + responsiveness

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {
   Switch,
   Route,
   useHistory,
+  Link,
 } from "react-router-dom";
 import SideNav from './SideNav';
 import Recipe1 from './recipe1';
@@ -31,16 +32,21 @@ function Home() {
       <div className="welcometext">
         <h1 className="title">Mon Dessert Préféré</h1>
         <h2 className="description">Hi there! Welcome to Mon Dessert Préféré. Here you'll be able to learn how to make my top three favorite desserts: Coffee Macarons, Cheesecake, and Chocolate Chip Cookies. I hope you enjoy baking and eating these desserts as much as I do! <br/><br/> Recipe 01 will bring you to the recipe for coffee macarons. Recipe 02 will bring you to the recipe for cheesecake, and Recipe 03 will bring you to the recipe for Chocolate Chip Cookies. </h2>
-        <SideNav/>
         <div className="buttondiv">
         <button className="navbuttons" onClick={() => history.push('/recipe1')}>Recipe 01</button>
         <button className="navbuttons" onClick={() => history.push('/recipe2')}>Recipe 02</button>
         <button className="navbuttons" onClick={() => history.push('/recipe3')}>Recipe 03</button>
         </div>
+        <div className="sidenav">
+         <Link to="/mondessertprefere" className="link">Home</Link>
+         <Link to="/recipe1" className="link">Recipe 01</Link>
+         <Link to="/recipe2" className="link">Recipe 02</Link>
+         <Link to="/recipe3" className="link">Recipe 03</Link>
+        </div>
         </div>
         <div className="welcomeimages">
-        <img className="homeimages" src="https://i.pinimg.com/originals/45/cd/50/45cd500e59a73630fe6e2f4b7f89a1bf.jpg" alt="Coffee Macarons"/>
-        <img className="homeimages" src="https://i.pinimg.com/564x/4f/96/ee/4f96ee7011d5eb459a6dacb49c5f09aa.jpg" alt="Choclate Chip Cookies"/>
+        <img className="homeimages" src="https://i.pinimg.com/originals/45/cd/50/45cd500e59a73630fe6e2f4b7f89a1bf.jpg" alt="Coffee Macarons"/><br/>
+        <img className="homeimages" src="https://i.pinimg.com/564x/4f/96/ee/4f96ee7011d5eb459a6dacb49c5f09aa.jpg" alt="Choclate Chip Cookies"/><br/>
         <img className="homeimages" src="https://data.whicdn.com/images/317268829/original.jpg" alt="Cheesecake"/>
         </div>
     </div>
